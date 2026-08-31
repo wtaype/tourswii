7dde5c060d1ca922a7c1f59e629145cc913102da
Wilder Taype <i201418741@cibertec.edu.pe>
2026-08-31 01:04:56 -0500

    Actualizacion Principal v1.0.8

--- src/wii.js
+++ src/wii.js
@@ -2,11 +2,10 @@
 export const id = 'tours-web';
 export const app = 'Sandboard Huacachina Evolution';
 export const by = '@wilder.taype';
-export const linkweb = 'https://tours.amorwii.com';
+export const linkweb = 'https://tourswii.amorwii.workers.dev/';
 export const linkme = 'https://wtaype.github.io/';
 export const version = 1.0;
 export const versionName = '1.0.0';
-export const dtema = 'futuro';
 
 export default {
   id,
@@ -15,6 +14,5 @@ export default {
   linkweb,
   linkme,
   version,
-  versionName,
-  dtema
+  versionName
 };
